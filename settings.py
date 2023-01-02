09fedf602f4c9750d7a0bd0e51be8d5645ba4c6d
DeepikaAuropro <111051092+DeepikaAuropro@users.noreply.github.com>
2023-01-02 11:27:21 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+class Settings():
+
+    def __init__(self):
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230, 240, 130)
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+        self.bullet_speed_factor = 2
+        self.bullet_width = 2
+        self.bullet_height = 10
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 4
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
+
+
+
